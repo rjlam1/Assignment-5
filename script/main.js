8585c03f9886e36768264e4b85357c281dff4031
shafiul islam laam <rjlam0514@gmail.com>
2025-03-02 00:50:57 +0600

    added html and js file

---
+++ script/main.js
@@ -0,0 +1,5 @@
+
+document.getElementById("backButton").addEventListener("click",function(event){
+   window.location.href="index.html"
+    
+})
--- script/script.js
+++ script/script.js
@@ -1,31 +1,71 @@
-const ringButtons=document.querySelectorAll(".ring-button");
-for (let btn of ringButtons){
-    btn.addEventListener('click',function(event){
-        const navNum=document.getElementById('nav-num');
-        let QuantityNav=parseInt(navNum.innerText);
-        
-        const leftQuantity=document.getElementById('left-Quantity');
-        let  Quantity=parseInt(leftQuantity.innerText)||0;
-        if(confirm("Board update Successfully")){
-            if(Quantity>0){
-                Quantity--;
-                leftQuantity.innerText=Quantity;
-                QuantityNav++;
-                navNum.innerText=QuantityNav;
 
-                btn.disabled=true;
-                btn.style.opacity=0.15;
+document.getElementById("img-Btn").addEventListener("click",function(){
+    const randomColor="#"+Math.floor(Math.random()*16777215).toString(16);
+    document.body.style.backgroundColor=randomColor;
+    
+});
+
+
+const ringButtons = document.querySelectorAll(".ring-button");
+
+for (let btn of ringButtons) {
+    btn.addEventListener('click', function(event) {
+        const navNum = document.getElementById('nav-num');
+        let QuantityNav = parseInt(navNum.innerText);
+        const titles=document.querySelector("title")
+        const leftQuantity = document.getElementById('left-Quantity');
+        let Quantity = parseInt(leftQuantity.innerText);
+
+        if (confirm("Board update Successfully")) {
+            if (Quantity > 0) {
+                Quantity--;
+                leftQuantity.innerText = Quantity;
+                QuantityNav++;
+                navNum.innerText = QuantityNav;
+
+                btn.disabled = true;
+                btn.style.opacity = 0.15;
+                const historyContainer = document.getElementById("history-container");
+                historyContainer.innerHTML += `
+                    <div class="p-4 my-2 bg-[#F4F7FF] rounded-3xl">
+                        <p class="font-medium">
+                            You have completed the task  at ${new Date().toLocaleTimeString()}
+                        </p>
+                    </div>`;
                 
             }
-            
         }
+
         
-    })
-};
+
+    });
+
+
+
+}
+
+document.getElementById("clears").addEventListener("click",function(){
+    const clearsBtn=document.getElementById("history-container")
+    clearsBtn.classList.add("hidden")
+})
+
+
+
+
+
+
+
+
+
 document.getElementById('text-Button').addEventListener('click', function() {
     window.location.href = "main.html"; 
 });
 
+
+
+document.getElementById('text-Button').addEventListener('click', function() {
+    window.location.href = "main.html"; 
+});
 
 
 
@@ -49,3 +89,10 @@ function updateDateAndDay() {
 
 
 updateDateAndDay();
+
+
+
+
+function historysAdd(title,time){
+   
+}
